3c6495e1fa0dc8f43bc66b20de5a6ec7f477f058
Matheus Rian <silva.matheus200107@gmail.com>
2025-04-30 19:36:43 -0300

    reload cache

--- src/hooks/useTask.tsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { TaskContext } from "../contexts/taskContext";
-
-export function useTask() {
-    return useContext(TaskContext)
-}
--- src/utils/updateDate3HoursAgo.ts
+++
@@ -1,5 +0,0 @@
-export function updateDate3HoursAgo(date: Date) {
-  const updateDate = new Date(date.getTime() - 1000 * 60 * 60 * 3); // 3h
-
-  return updateDate;
-}
